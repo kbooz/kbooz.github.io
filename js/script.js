0ba411c28751fcab1c59cdf15dd9dcc7485ccaca
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2015-06-25 02:55:35 -0300

    update

--- js/script.js
+++ js/script.js
@@ -110,7 +110,7 @@ $(".fruta").attr("src",function(){
   console.log(atribute);
   var fruta = pickFruit(getUrlParameter("fruta"));
   text += fruta + " " + atribute + "\n";
-  $("#orga");
+  $("#overlay-harmonia").text(text);
   return "img/svg/"+fruta+"_"+atribute+".svg";
 });
 
