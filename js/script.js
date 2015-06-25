fa474d5c3a874712cffe8b03c88c0d4a56c1c541
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2015-06-25 02:44:37 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -3,9 +3,12 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
+ var frutas = ["laranja","pimentao","morango"];
+ var atributes = ["fechado","feliz","surpreso","convencional"];
 
 (function($) {
     "use strict"; // Start of use strict
+
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
@@ -35,6 +38,13 @@
         }
     );
 
+    $(".overlay-content-box").fitText(
+        1.2, {
+            minFontSize: '35px',
+            maxFontSize: '65px'
+        }
+    );
+
     // Offset for Main Navigation
     $('#mainNav').affix({
         offset: {
@@ -45,4 +55,80 @@
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
+    $(".fruta").attr("src",function(){
+    	var atribute = getAtribute($(this).attr("class").split(" "));
+    	console.log(atribute);
+    	var fruta = pickFruit(getUrlParameter("fruta"));
+    	return "img/svg/"+fruta+"_"+atribute+".svg";
+    });
+
 })(jQuery); // End of use strict
+
+function randomFruit()
+{
+	var num = Math.floor((Math.random() * frutas.length));
+	return frutas[num];
+}
+
+function pickFruit(fruta)
+{
+	if (isFruit(fruta))
+		return fruta;
+	else
+		return randomFruit();
+}
+
+function isFruit(fruta)
+{
+	for (var i = 0; i < frutas.length; i++) {
+		if (frutas[i] == fruta)
+			return true;
+	}
+	return false;
+}
+
+function getAtribute(attrs)
+{
+	for (var e = 0; e < attrs.length; e++) {
+		for (var i = 0; i < atributes.length; i++) {
+			if (attrs[e] == atributes[i])
+				return atributes[i];
+		}
+	}
+	return false;
+}
+
+function getUrlParameter(sParam)
+{
+	var sPageURL = window.location.search.substring(1);
+	var sURLVariables = sPageURL.split('&');
+	for (var i = 0; i < sURLVariables.length; i++)
+	{
+			var sParameterName = sURLVariables[i].split('=');
+			if (sParameterName[0] == sParam)
+			{
+					return sParameterName[1];
+			}
+	}
+}
+
+$("#link-harmonia").click(function(){
+  $("#overlay-wrapper").show();
+  $("#organico-harmonia").show();
+});
+
+$("#link-diferenca").click(function(){
+  $("#overlay-wrapper").show();
+  $("#organico-diferenca").show();
+});
+
+$("#link-concentracao").click(function(){
+  $("#overlay-wrapper").show();
+  $("#organico-concentracao").show();
+});
+
+$("#hide").click(function(){
+  $("#overlay-wrapper").hide();
+  $("#overlay-wrapper").children().hide();
+  $("#hide").show();
+})
