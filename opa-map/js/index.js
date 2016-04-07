a96323a932c06f973d502c965adb8c677cc147bd
Lucas Inacio de Souza <lucas1inacio7souza@gmail.com>
2016-04-07 09:03:12 -0300

    Updated Opa-map

--- opa-map/js/index.js
+++ opa-map/js/index.js
@@ -28,15 +28,15 @@ angular.module('ionic.example', ['ionic'])
 	};
 
 	$scope.distance = function(i) {
-		return getDistance($scope.markers[i].position,$scope.myselfMarker.position).toString()
+		return checkDistance($scope.markers[i],$scope.myselfMarker).toString()
 	}
 
 	function initialize()
 	{
-		
+
 		// Define a posição
 		var myLatlng = new google.maps.LatLng(-22.962868,-43.170048);
-		
+
 		// Opções do Mapa
 		var mapOptions = {
 			center: myLatlng,
@@ -85,8 +85,7 @@ angular.module('ionic.example', ['ionic'])
 	function createMarkes()
 	{
 		//Lugares pré definidos
-		var places = [[-22.964357,-43.177534],[-22.963381,-43.172658],[-22.963253,-43.17494],[-22.966842,-43.171528],[-22.965439,-43.172273],[-22.968099,-43.172221],[-22.969836,-43.177189],[-22.967079,-43.173814],[-22.965786,-43.173986],[-22.961722,-43.173604],[-22.967147,-43.171253],[-22.969799,-43.177486],[-22.96523,-43.173245],[-22.967918,-43.174884],[-22.967695,-43.178544],[-22.967877,-43.172252]];
-
+		var places = getBizz();
 
 		for (var i = 0; i <places.length; i++) {
 			$scope.markers[i] = new google.maps.Marker({
@@ -100,34 +99,29 @@ angular.module('ionic.example', ['ionic'])
 		for (var i = 0; i <$scope.markers.length; i++) {
 			var contentString = "<div>Distância em metros:<br><span ng-bind='distance("+i+")'></span></div>";
 			var compiled = $compile(contentString)($scope);
-			google.maps.event.addListener($scope.markers[i], 'click', (function(map,marker,content,infowindow){ 
-				return function() {
-					infowindow.setContent(content);
-					infowindow.open(map,marker);
-				};
-			})($scope.map,$scope.markers[i],compiled[0],infowindow));
+			google.maps.event.addListener($scope.markers[i], 'click', (createInfoWindow)($scope.map,$scope.markers[i],compiled[0],infowindow));
 		}
-
-		/*
-				//Marker + infowindow + angularjs compiled ng-click
-		var contentString = "<div><a ng-click='clickTest()'>Clique em mim!</a></div>";
-		var compiled = $compile(contentString)($scope);
-
-		var infowindow = new google.maps.InfoWindow({
-			content: compiled[0]
-		});
-
-		google.maps.event.addListener(myselfMarker, 'click', function() {
-			infowindow.open(map,myselfMarker);
-		});
-		*/
 	}
+
+	var getBizz = function(){
+		// Demo
+		return [[-22.964357,-43.177534],[-22.963381,-43.172658],[-22.963253,-43.17494],[-22.966842,-43.171528],[-22.965439,-43.172273],[-22.968099,-43.172221],[-22.969836,-43.177189],[-22.967079,-43.173814],[-22.965786,-43.173986],[-22.961722,-43.173604],[-22.967147,-43.171253],[-22.969799,-43.177486],[-22.96523,-43.173245],[-22.967918,-43.174884],[-22.967695,-43.178544],[-22.967877,-43.172252]];
+	}
+
+	var createInfoWindow = function(map,marker,content,infowindow){ 
+		return function() {
+			infowindow.setContent(content);
+			infowindow.open(map,marker);
+		};
+	};
 
 	var rad = function(x){
 		return x * Math.PI / 180;
 	};
 
-	var getDistance = function(p1, p2) {
+	var checkDistance = function(m1, m2) {
+		var p1 = m1.position;
+		var p2 = m2.position;
 		var R = 6378137; // Earth’s mean radius in meter
 		var dLat = rad(p2.lat() - p1.lat());
 		var dLong = rad(p2.lng() - p1.lng());
@@ -136,7 +130,13 @@ angular.module('ionic.example', ['ionic'])
 		Math.sin(dLong / 2) * Math.sin(dLong / 2);
 		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 		var d = R * c;
-		return ~~d; // returns the distance in meter /// ~~ -> transforma o float no inteiro mais próximo
+		d= ~~ d;  /// ~~ -> transforma o float no inteiro mais próximo
+		if(d<50 && m1.label != "x")
+		{
+			m1.set('label','x');
+			console.log(m1);
+		}
+		return ~~d; // returns the distance in meter
 	};
 
 });
